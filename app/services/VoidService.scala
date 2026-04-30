0a89b49e8112425cd39931a42652899640f22b6b
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2026-04-30 15:57:33 +0100

    DAC4212 - TEST CHANGE

--- app/services/VoidService.scala
+++ app/services/VoidService.scala
@@ -6,9 +6,9 @@ import uk.gov.hmrc.http.HeaderCarrier
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
-class VoidService @Inject() (fatcaConnector: FatcaVoidConnector)(implicit ec: ExecutionContext, hc: HeaderCarrier) {
+class VoidService @Inject() (fatcaConnector: FatcaVoidConnector) {
 
-  def fatcaVoid(messageRefId: String, fiid: String): Unit = {
+  def fatcaVoid(messageRefId: String, fiid: String)(using hc: HeaderCarrier, ec: ExecutionContext): Unit = {
     val request = models.VoidFatcaRequest(messageRefId, fiid)
     fatcaConnector.submit(request)
   }
--- test/services/VoidServiceSpec.scala
+++ test/services/VoidServiceSpec.scala
@@ -8,15 +8,17 @@ import org.mockito.Mockito.{verify, when}
 import uk.gov.hmrc.http.HeaderCarrier
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 class VoidServiceSpec extends SpecBase {
   private val mockConnector = mock[FatcaVoidConnector]
   private val service       = new VoidService(mockConnector)
 
+  given HeaderCarrier = HeaderCarrier()
+
   "fatcaVoid" - {
     "should call the connector with the correct request" ignore {
-      when(mockConnector.submit(any[VoidFatcaRequest]())(any[HeaderCarrier](), any[ExecutionContext]())).thenReturn(Future.successful(()))
+      when(mockConnector.submit(any[VoidFatcaRequest]())).thenReturn(Future.successful(()))
 
       service.fatcaVoid("testMessageRefId", "testFiid")
 
