0a92f2f00d1f323a40a876dad7dbb88f29877b7d
Carl Thompson <carl.thompson@opencastsoftware.com>
2026-05-08 10:56:23 +0100

    resolve duplicate url in config

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -33,19 +33,18 @@ class FrontendAppConfig @Inject() (configuration: Configuration, servicesConfig:
   def feedbackUrl(implicit request: RequestHeader): String =
     s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${host + request.uri}"
 
-  val loginUrl: String         = configuration.get[String]("urls.login")
-  val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
-  val signOutUrl: String       = configuration.get[String]("urls.signOut")
-  val fileUploadUrl: String    = configuration.get[String]("urls.fileUpload")
-  val manageYourFisUrl: String = configuration.get[String]("urls.manageYourFis")
+  val loginUrl: String          = configuration.get[String]("urls.login")
+  val loginContinueUrl: String  = configuration.get[String]("urls.loginContinue")
+  val signOutUrl: String        = configuration.get[String]("urls.signOut")
+  val fileUploadUrl: String     = configuration.get[String]("urls.fileUpload")
+  val manageReportsLink: String = configuration.get[String]("urls.manageFI")
 
   private val exitSurveyBaseUrl: String = configuration.get[Service]("microservice.services.feedback-frontend").baseUrl
-  val exitSurveyUrl: String             = s"$exitSurveyBaseUrl/feedback/crs-fatca-manual-submission-frontend"
 
-  val timeout: Int   = configuration.get[Int]("timeout-dialog.timeout")
-  val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
+  val exitSurveyUrl: String = s"$exitSurveyBaseUrl/feedback/crs-fatca-manual-submission-frontend"
+  val timeout: Int          = configuration.get[Int]("timeout-dialog.timeout")
 
-  val manageReportsLink: String        = configuration.get[String]("urls.manageFI")
+  val countdown: Int                   = configuration.get[Int]("timeout-dialog.countdown")
   val cacheTtl: Long                   = configuration.get[Int]("mongodb.timeToLiveInSeconds")
   val enrolmentKey: String             = configuration.get[String]("keys.enrolmentKey.crsFatca")
   lazy val registerUrl: String         = configuration.get[String]("urls.register")
