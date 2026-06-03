9930ade78950e4029ce5b3c63c532d72ef9806f7
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2026-06-03 12:50:54 +0100

    DAC6-4277 | add spec

--- test/controllers/elections/CheckYourAnswersControllerSpec.scala
+++ test/controllers/elections/CheckYourAnswersControllerSpec.scala
@@ -19,11 +19,17 @@ package controllers.elections
 import base.SpecBase
 import controllers.routes
 import models.{FiIdentifiers, UserAnswers}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
 import pages.FiDetailsPage
+import play.api.inject
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
+import services.ElectionsService
 import viewmodels.govuk.SummaryListFluency
 import views.html.CheckYourAnswersView
+
+import scala.concurrent.Future
 
 class CheckYourAnswersControllerSpec extends SpecBase with SummaryListFluency {
 
@@ -71,5 +77,33 @@ class CheckYourAnswersControllerSpec extends SpecBase with SummaryListFluency {
         redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
       }
     }
+
+    "onSubmit" - {
+
+      val onSubmitUrl = controllers.elections.routes.CheckYourAnswersController.onSubmit(2026).url
+      "must redirect when service return success" in {
+
+        val mockService = mock[ElectionsService]
+
+        when(mockService.submit(any(), any())(any())) thenReturn Future.successful(())
+
+        val application =
+          applicationBuilder(userData = Some(emptyUserAnswers))
+            .overrides(
+              inject.bind[ElectionsService].toInstance(mockService)
+            )
+            .build()
+
+        running(application) {
+          val request = FakeRequest(POST, onSubmitUrl)
+
+          val result = route(application, request).value
+
+          status(result) mustEqual SEE_OTHER
+          redirectLocation(result).value mustEqual controllers.elections.routes.ElectionsSentController.onPageLoad().url
+        }
+      }
+    }
+
   }
 }
