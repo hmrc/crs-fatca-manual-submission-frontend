8294eac2361705cfec8a8df17e4ddb07f925a50a
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2026-07-02 16:39:57 +0100

    DAC6-4362 | redirect to sponsor section

--- app/models/viewModels/SendAReport.scala
+++ app/models/viewModels/SendAReport.scala
@@ -16,6 +16,8 @@
 
 package models.viewModels
 
+import models.NormalMode
+
 sealed trait TaskStatus
 
 object TaskStatus {
@@ -37,40 +39,49 @@ final case class SendAReportSections(
   tbc2: Option[TaskStatus]
 ) {
 
-  private def task(titleKey: String, status: Option[TaskStatus]): Seq[SendAReportTask] =
-    status.map(SendAReportTask(titleKey, _)).toSeq
+  private def task(titleKey: String, status: Option[TaskStatus], href: Option[String]): Seq[SendAReportTask] =
+    status.map(SendAReportTask(titleKey, _, href)).toSeq
 
   val sections: Seq[SendAReportSection] =
     Seq(
       SendAReportSection(
         headingKey = "sendAReport.reportDetails.heading",
         idPrefix = "report-details",
-        tasks = task("sendAReport.reportDetails.subHeading", reportDetails)
+        tasks = task("sendAReport.reportDetails.subHeading", reportDetails, href = Some(controllers.routes.UnderConstructionController.onPageLoad().url))
       ),
       SendAReportSection(
         headingKey = "sendAReport.financialInstitution.heading",
         idPrefix = "financial-institution",
-        tasks = task("sendAReport.financialInstitution.subHeading", financialInstitutionDetails)
+        tasks = task("sendAReport.financialInstitution.subHeading",
+                     financialInstitutionDetails,
+                     href = Some(controllers.routes.UnderConstructionController.onPageLoad().url)
+        )
       ),
       SendAReportSection(
         headingKey = "sendAReport.sponsor.heading",
         idPrefix = "sponsor",
-        tasks = task("sendAReport.sponsor.subHeading", sponsorDetails)
+        tasks = task("sendAReport.sponsor.subHeading",
+                     sponsorDetails,
+                     href = Some(controllers.manual.sponsor.routes.HaveSponsorController.onPageLoad(NormalMode).url)
+        )
       ),
       SendAReportSection(
         headingKey = "sendAReport.filerCategory.heading",
         idPrefix = "filer-category",
-        tasks = task("sendAReport.filerCategory.subHeading", filerCategory)
+        tasks = task("sendAReport.filerCategory.subHeading", filerCategory, href = Some(controllers.routes.UnderConstructionController.onPageLoad().url))
       ),
       SendAReportSection(
         headingKey = "sendAReport.accountsInformation.heading",
         idPrefix = "accounts-information",
         tasks = Seq(
-          task("sendAReport.accountsInformation.accounts", accounts),
-          task("sendAReport.accountsInformation.accountHolders", accountHolders),
-          task("sendAReport.accountsInformation.controllingPersons", controllingPersons),
-          task("sendAReport.accountsInformation.tbc1", tbc1),
-          task("sendAReport.accountsInformation.tbc2", tbc2)
+          task("sendAReport.accountsInformation.accounts", accounts, href = Some(controllers.routes.UnderConstructionController.onPageLoad().url)),
+          task("sendAReport.accountsInformation.accountHolders", accountHolders, href = Some(controllers.routes.UnderConstructionController.onPageLoad().url)),
+          task("sendAReport.accountsInformation.controllingPersons",
+               controllingPersons,
+               href = Some(controllers.routes.UnderConstructionController.onPageLoad().url)
+          ),
+          task("sendAReport.accountsInformation.tbc1", tbc1, href = Some(controllers.routes.UnderConstructionController.onPageLoad().url)),
+          task("sendAReport.accountsInformation.tbc2", tbc2, href = Some(controllers.routes.UnderConstructionController.onPageLoad().url))
         ).flatten
       )
     ).filter(_.tasks.nonEmpty)
@@ -84,5 +95,6 @@ final case class SendAReportSection(
 
 final case class SendAReportTask(
   titleKey: String,
-  status: TaskStatus
+  status: TaskStatus,
+  href: Option[String]
 )
