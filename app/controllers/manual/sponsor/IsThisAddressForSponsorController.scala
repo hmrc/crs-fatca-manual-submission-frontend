393e456e8a726346b81fd1a7f74a8485b2645492
Carl Thompson <carl.thompson@opencastsoftware.com>
2026-07-20 13:05:47 +0100

    DAC6-4390 | create isThisAddressForSponsor page

---
+++ app/controllers/manual/sponsor/IsThisAddressForSponsorController.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.manual.sponsor
+
+import controllers.actions._
+import forms.manual.sponsor.IsThisAddressForSponsorFormProvider
+import javax.inject.Inject
+import models.{Mode, ReportId}
+import navigation.ManualSubmissionNavigator
+import pages.manual.sponsor.IsThisAddressForSponsorPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import connectors.DatabaseConnector
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.manual.sponsor.IsThisAddressForSponsorView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class IsThisAddressForSponsorController @Inject()(
+                                         override val messagesApi: MessagesApi,
+                                         repository: DatabaseConnector,
+                                         navigator: ManualSubmissionNavigator,
+                                         identify: IdentifierAction,
+                                         getData: DataRetrievalAction,
+                                         requireData: DataRequiredAction,
+                                         reportIdAction: ReportIdRequiredAction,
+                                         formProvider: IsThisAddressForSponsorFormProvider,
+                                         val controllerComponents: MessagesControllerComponents,
+                                         view: IsThisAddressForSponsorView
+                                 )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  val form = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData andThen reportIdAction) {
+    implicit request =>
+
+      implicit val reportId: ReportId = request.reportId
+
+      val preparedForm = request.userAnswers.get(IsThisAddressForSponsorPage()) match {
+        case None => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, mode))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData andThen reportIdAction).async {
+    implicit request =>
+
+      implicit val reportId: ReportId = request.reportId
+
+      form.bindFromRequest().fold(
+        formWithErrors =>
+          Future.successful(BadRequest(view(formWithErrors, mode))),
+
+        value =>
+          for {
+            updatedAnswers <- Future.fromTry(request.userAnswers.setWithReportId(IsThisAddressForSponsorPage(), value))
+            _              <- repository.set(updatedAnswers)
+          } yield Redirect(navigator.nextPage(IsThisAddressForSponsorPage(), mode, updatedAnswers))
+      )
+  }
+}
