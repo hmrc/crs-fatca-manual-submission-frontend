3e58aeeaab56edfa30e30a97aa5881f0cfd955fb
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2026-07-22 15:18:47 +0100

    DAC6-4391 - autocomplete js cleanup

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,38 +1,3 @@
-// =====================================================
-// Back link mimics browser back functionality
-// =====================================================
-// store referrer value to cater for IE - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/10474810/  */
-var docReferrer = document.referrer
-// prevent resubmit warning
-if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
-    window.history.replaceState(null, null, window.location.href);
-}
-
-// handle back click
-var backLink = document.querySelector('.govuk-back-link');
-if (backLink !== null) {
-    backLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        window.history.back();
-    });
-}
-
-// Find first ancestor of el with tagName
-// or undefined if not found
-function upTo(el, tagName) {
-    tagName = tagName.toLowerCase();
-
-    while (el && el.parentNode) {
-        el = el.parentNode;
-        if (el.tagName && el.tagName.toLowerCase() == tagName) {
-            return el;
-        }
-    }
-
-    return null;
-}
-
 var countrySelect = document.querySelector('select#country');
 if (countrySelect !== null) {
     // Prevent HMRC module auto-init so we control initialisation ourselves
