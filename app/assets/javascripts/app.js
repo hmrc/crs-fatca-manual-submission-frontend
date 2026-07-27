fad196e01ce0f43edab8573b096a03588c1a1261
witchelljack <36855484+WitchellJack@users.noreply.github.com>
2026-07-27 09:29:25 +0100

    DAC6-4382 - resolved issue with JS not submitting field correctly

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,168 +1,253 @@
 (function () {
     'use strict';
 
-    function initialiseCountryAutocomplete() {
-        var countrySelect = document.querySelector('select#country');
+    var countrySelect = document.querySelector('select#country');
 
-        if (countrySelect === null) {
-            return;
-        }
+    if (
+        countrySelect === null ||
+        typeof window.HMRCAccessibleAutocomplete === 'undefined'
+    ) {
+        return;
+    }
 
-        if (
-            typeof window.HMRCAccessibleAutocomplete === 'undefined' ||
-            countrySelect.getAttribute('data-country-autocomplete-initialised') === 'true'
-        ) {
-            return;
-        }
+    countrySelect.removeAttribute('data-module');
 
-        countrySelect.removeAttribute('data-module');
+    var countryOptions = countrySelect.options;
+    var countries = [];
 
-        var countryOptions = countrySelect.options;
+    function normalise(value) {
+        return (value || '').toLowerCase().trim();
+    }
 
-        function getSearchTerms(option) {
-            var dataText = option.getAttribute('data-text');
-            var rawTerms = dataText
-                ? dataText.split(':')
-                : [option.textContent || ''];
+    function getSearchTerms(option) {
+        var dataText = option.getAttribute('data-text');
 
-            var terms = [];
+        var rawTerms = dataText
+            ? dataText.split(':')
+            : [option.textContent || ''];
 
-            for (var i = 0; i < rawTerms.length; i++) {
-                var term = rawTerms[i].trim();
+        var terms = [];
 
-                if (term && terms.indexOf(term) === -1) {
-                    terms.push(term);
-                }
+        for (var i = 0; i < rawTerms.length; i++) {
+            var term = rawTerms[i].trim();
+
+            if (term && terms.indexOf(term) === -1) {
+                terms.push(term);
             }
-
-            return terms;
-        }
-        function getDisplayName(option) {
-            var terms = getSearchTerms(option);
-
-            return terms.length > 0
-                ? terms[0]
-                : option.textContent.trim();
         }
 
-        function getDefaultValue() {
-            var selectedOption =
-                countrySelect.options[countrySelect.selectedIndex];
+        return terms;
+    }
 
-            if (!selectedOption || !selectedOption.value) {
-                return '';
-            }
+    function getDisplayName(option) {
+        var searchTerms = getSearchTerms(option);
 
-            return getDisplayName(selectedOption);
+        return searchTerms.length > 0
+            ? searchTerms[0]
+            : option.textContent.trim();
+    }
+
+    for (var i = 0; i < countryOptions.length; i++) {
+        var option = countryOptions[i];
+
+        if (!option.value) {
+            continue;
         }
 
-        window.HMRCAccessibleAutocomplete.enhanceSelectElement({
-            selectElement: countrySelect,
-            defaultValue: getDefaultValue(),
-            showAllValues:
-                countrySelect.getAttribute('data-show-all-values') === 'true',
-            autoselect:
-                countrySelect.getAttribute('data-auto-select') === 'true',
-            minLength: 2,
-
-            source: function (query, syncResults) {
-                var normalisedQuery = (query || '').toLowerCase().trim();
-
-                if (!normalisedQuery) {
-                    syncResults([]);
-                    return;
-                }
-
-                var exactAliasMatches = [];
-                var partialMatches = [];
-
-                for (var i = 0; i < countryOptions.length; i++) {
-                    var option = countryOptions[i];
-
-                    if (!option.value) {
-                        continue;
-                    }
-
-                    var searchTerms = getSearchTerms(option);
-                    var displayName = getDisplayName(option);
-                    var exactAliasMatch = false;
-                    var partialMatch = false;
-
-                    for (var j = 0; j < searchTerms.length; j++) {
-                        var normalisedTerm = searchTerms[j]
-                            .toLowerCase()
-                            .trim();
-
-                        if (
-                            j > 0 &&
-                            normalisedTerm === normalisedQuery
-                        ) {
-                            exactAliasMatch = true;
-                        }
-
-                        if (
-                            normalisedTerm.indexOf(normalisedQuery) !== -1
-                        ) {
-                            partialMatch = true;
-                        }
-                    }
-
-                    if (
-                        exactAliasMatch &&
-                        exactAliasMatches.indexOf(displayName) === -1
-                    ) {
-                        exactAliasMatches.push(displayName);
-                    } else if (
-                        partialMatch &&
-                        partialMatches.indexOf(displayName) === -1
-                    ) {
-                        partialMatches.push(displayName);
-                    }
-                }
-
-                syncResults(
-                    exactAliasMatches.length > 0
-                        ? exactAliasMatches
-                        : partialMatches
-                );
-            },
-
-            onConfirm: function (selected) {
-                var selectedDisplayName =
-                    typeof selected === 'string'
-                        ? selected
-                        : '';
-
-                if (!selectedDisplayName) {
-                    countrySelect.value = '';
-                    return;
-                }
-
-                for (var i = 0; i < countryOptions.length; i++) {
-                    var option = countryOptions[i];
-                    var candidate = getDisplayName(option);
-
-                    if (candidate === selectedDisplayName) {
-                        countrySelect.value = option.value;
-                        option.selected = true;
-                        return;
-                    }
-                }
-            }
+        countries.push({
+            name: getDisplayName(option),
+            value: option.value,
+            searchTerms: getSearchTerms(option)
         });
+    }
 
-        countrySelect.setAttribute(
-            'data-country-autocomplete-initialised',
-            'true'
+    function getDefaultValue() {
+        var selectedOption =
+            countrySelect.options[countrySelect.selectedIndex];
+
+        if (!selectedOption || !selectedOption.value) {
+            return '';
+        }
+
+        return getDisplayName(selectedOption);
+    }
+
+    function findCountryByInput(value) {
+        var normalisedValue = normalise(value);
+
+        if (!normalisedValue) {
+            return null;
+        }
+
+        for (var i = 0; i < countries.length; i++) {
+            var country = countries[i];
+
+            for (var j = 0; j < country.searchTerms.length; j++) {
+                if (
+                    normalise(country.searchTerms[j]) === normalisedValue
+                ) {
+                    return country;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    function setSelectedCountry(country) {
+
+        for (var i = 0; i < countryOptions.length; i++) {
+            countryOptions[i].selected = false;
+        }
+
+        if (!country) {
+            countrySelect.value = '';
+
+            if (countryOptions.length > 0) {
+                countryOptions[0].selected = true;
+            }
+
+            return;
+        }
+
+        countrySelect.value = country.value;
+
+        for (var j = 0; j < countryOptions.length; j++) {
+            if (countryOptions[j].value === country.value) {
+                countryOptions[j].selected = true;
+                break;
+            }
+        }
+
+        /*
+         * Dispatch a change event in case anything else on the page is
+         * listening for changes to the original select.
+         */
+        countrySelect.dispatchEvent(
+            new Event('change', {
+                bubbles: true
+            })
         );
     }
 
-    if (document.querySelector('select#country') !== null) {
-        initialiseCountryAutocomplete();
-    } else {
-        document.addEventListener(
-            'DOMContentLoaded',
-            initialiseCountryAutocomplete
+    window.HMRCAccessibleAutocomplete.enhanceSelectElement({
+        selectElement: countrySelect,
+        defaultValue: getDefaultValue(),
+
+        showAllValues:
+            countrySelect.getAttribute('data-show-all-values') === 'true',
+
+        autoselect:
+            countrySelect.getAttribute('data-auto-select') === 'true',
+
+        minLength: 2,
+
+        source: function (query, syncResults) {
+            var normalisedQuery = normalise(query);
+
+            if (!normalisedQuery) {
+                syncResults([]);
+                return;
+            }
+
+            var exactMatches = [];
+            var partialMatches = [];
+
+            for (var i = 0; i < countries.length; i++) {
+                var country = countries[i];
+                var exactMatch = false;
+                var partialMatch = false;
+
+                for (
+                    var j = 0;
+                    j < country.searchTerms.length;
+                    j++
+                ) {
+                    var searchTerm =
+                        normalise(country.searchTerms[j]);
+
+                    if (searchTerm === normalisedQuery) {
+                        exactMatch = true;
+                    }
+
+                    if (
+                        searchTerm.indexOf(normalisedQuery) !== -1
+                    ) {
+                        partialMatch = true;
+                    }
+                }
+
+                if (exactMatch) {
+                    exactMatches.push(country);
+                } else if (partialMatch) {
+                    partialMatches.push(country);
+                }
+            }
+
+            syncResults(
+                exactMatches.length > 0
+                    ? exactMatches
+                    : partialMatches
+            );
+        },
+
+        templates: {
+            inputValue: function (country) {
+                return country
+                    ? country.name
+                    : '';
+            },
+
+            suggestion: function (country) {
+                return country.name;
+            }
+        },
+
+        onConfirm: function (country) {
+            setSelectedCountry(country || null);
+        }
+    });
+
+    var autocompleteInput =
+        document.querySelector('input#country');
+
+    if (autocompleteInput !== null) {
+
+        autocompleteInput.addEventListener(
+            'input',
+            function () {
+                var currentlySelected =
+                    countries.find(function (country) {
+                        return (
+                            country.value === countrySelect.value
+                        );
+                    });
+
+                if (
+                    currentlySelected &&
+                    normalise(this.value) !==
+                    normalise(currentlySelected.name)
+                ) {
+                    setSelectedCountry(null);
+                }
+            }
+        );
+    }
+
+
+    if (countrySelect.form !== null) {
+        countrySelect.form.addEventListener(
+            'submit',
+            function () {
+                if (autocompleteInput === null) {
+                    return;
+                }
+
+                var country =
+                    findCountryByInput(autocompleteInput.value);
+
+                setSelectedCountry(country);
+            }
         );
     }
 })();
