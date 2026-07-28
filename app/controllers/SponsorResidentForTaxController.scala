cbcc641a604e01641faf1e36d24f1f16ff8d782c
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2026-07-28 18:02:52 +0100

    DAC6-4405 | CRS/FATCA : Create "resident-for-tax" page (Sponsor section, Manual)

---
+++ app/controllers/SponsorResidentForTaxController.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.*
+import forms.SponsorResidentForTaxFormProvider
+
+import javax.inject.Inject
+import models.{Mode, ReportId}
+import navigation.ManualSubmissionNavigator
+import pages.SponsorResidentForTaxPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import connectors.DatabaseConnector
+import pages.manual.sponsor.SponsorNamePage
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.SponsorResidentForTaxView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class SponsorResidentForTaxController @Inject() (
+  override val messagesApi: MessagesApi,
+  repository: DatabaseConnector,
+  navigator: ManualSubmissionNavigator,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  reportIdAction: ReportIdRequiredAction,
+  formProvider: SponsorResidentForTaxFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: SponsorResidentForTaxView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  val form = formProvider()
+
+  def onPageLoad(mode: Mode, idx: Option[Int] = None): Action[AnyContent] = (identify andThen getData andThen requireData andThen reportIdAction) {
+    implicit request =>
+
+      implicit val reportId: ReportId = request.reportId
+
+      request.userAnswers
+        .get(SponsorNamePage())
+        .fold(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad().url)) {
+          sponsorName =>
+            val preparedForm = request.userAnswers.get(SponsorResidentForTaxPage()) match {
+              case None        => form
+              case Some(value) => form.fill(value)
+            }
+
+            Ok(view(preparedForm, mode, sponsorName))
+        }
+  }
+
+  def onSubmit(mode: Mode, idx: Option[Int] = None): Action[AnyContent] = (identify andThen getData andThen requireData andThen reportIdAction).async {
+    implicit request =>
+
+      implicit val reportId: ReportId = request.reportId
+      request.userAnswers.get(SponsorNamePage())
+        .fold(Future.successful(Redirect(controllers.routes.JourneyRecoveryController.onPageLoad().url))) { sponsorName =>
+          form
+            .bindFromRequest()
+            .fold(
+              formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode, sponsorName))),
+              value =>
+                for {
+                  updatedAnswers <- Future.fromTry(request.userAnswers.setWithReportId(SponsorResidentForTaxPage(), value))
+                  _              <- repository.set(updatedAnswers)
+                } yield Redirect(navigator.nextPage(SponsorResidentForTaxPage(), mode, updatedAnswers))
+            )
+        }
+  }
+}
---
+++ test/controllers/SponsorResidentForTaxControllerSpec.scala
@@ -0,0 +1,146 @@
+package controllers
+
+import base.SpecBase
+import connectors.DatabaseConnector
+import forms.SponsorResidentForTaxFormProvider
+import models.SubmissionsConstants.CRS
+import models.{NormalMode, ReportId}
+import navigation.{FakeManualSubmissionNavigator, ManualSubmissionNavigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.{ReportIdPage, SponsorResidentForTaxPage}
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.SponsorResidentForTaxView
+
+import scala.concurrent.Future
+
+class SponsorResidentForTaxControllerSpec extends SpecBase with MockitoSugar {
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val formProvider = new SponsorResidentForTaxFormProvider()
+  val form         = formProvider()
+
+  lazy val sponsorResidentForTaxRoute = routes.SponsorResidentForTaxController.onPageLoad(NormalMode).url
+
+  "SponsorResidentForTax Controller" - {
+
+    val ua = emptyUserAnswers.withPage(ReportIdPage, ReportId(CRS, 2025, None, "TestfiID"))
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(maybeUserAnswers = Some(ua)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, sponsorResidentForTaxRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[SponsorResidentForTaxView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      implicit val reportId = ReportId(CRS, 2025, None, "TestfiID")
+
+      val userAnswers = ua.set(SponsorResidentForTaxPage(), "answer").success.value
+
+      val application = applicationBuilder(maybeUserAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, sponsorResidentForTaxRoute)
+
+        val view = application.injector.instanceOf[SponsorResidentForTaxView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill("answer"), NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to the next page when valid data is submitted" in {
+
+      val mockSessionRepository = mock[DatabaseConnector]
+
+      when(mockSessionRepository.set(any())(any())) thenReturn Future.successful(())
+
+      val application =
+        applicationBuilder(maybeUserAnswers = Some(ua))
+          .overrides(
+            bind[ManualSubmissionNavigator].toInstance(new FakeManualSubmissionNavigator(onwardRoute)),
+            bind[DatabaseConnector].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, sponsorResidentForTaxRoute)
+            .withFormUrlEncodedBody(("value", "answer"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onwardRoute.url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted" in {
+
+      val application = applicationBuilder(maybeUserAnswers = Some(ua)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, sponsorResidentForTaxRoute)
+            .withFormUrlEncodedBody(("value", ""))
+
+        val boundForm = form.bind(Map("value" -> ""))
+
+        val view = application.injector.instanceOf[SponsorResidentForTaxView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(maybeUserAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, sponsorResidentForTaxRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(maybeUserAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, sponsorResidentForTaxRoute)
+            .withFormUrlEncodedBody(("value", "answer"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
