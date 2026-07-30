31774186e830fb57700e1da53699874015a33c97
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2026-07-30 09:44:10 +0100

    DAC6-4402 | implement account closed page

--- test/navigation/ManualSubmissionNavigatorSpec.scala
+++ test/navigation/ManualSubmissionNavigatorSpec.scala
@@ -20,10 +20,10 @@ import base.SpecBase
 import controllers.manual.reportdetails.routes.{ReportDetailsCheckAnswersController, ReportingYearController, TypeOfReportController}
 import models.*
 import models.SubmissionsConstants.FATCA
+import models.response.{AddressLookup, Country}
 import models.viewModels.AccountId
-import models.response.{Address, AddressLookup, Country}
 import pages.*
-import pages.manual.account.{HaveNumberPage, IdentifierPage, NumberTypePage}
+import pages.manual.account.{AccountClosedPage, HaveNumberPage, IdentifierPage, NumberTypePage}
 import pages.manual.filercategory.{WhatTypeOfFilerIsSponsorPage, WhatTypeOfFilerPage}
 import pages.manual.reportdetails.{CrsOrFatcaPage, ReportingYearPage, TypeOfReportPage}
 import pages.manual.sponsor.*
@@ -208,9 +208,19 @@ class ManualSubmissionNavigatorSpec extends SpecBase {
 
       "IdentifierPage" - {
 
-        "must go to UnderConstruction Page when when answer is No" in {
+        "must go to AccountClosed Page" in {
           val userData = UserAnswers("id").withPage(IdentifierPage(accountId), "testId")
           navigator.nextPage(IdentifierPage(accountId), NormalMode, userData) mustBe
+            controllers.manual.account.routes.AccountClosedController.onPageLoad(NormalMode)
+        }
+
+      }
+
+      "AccountClosed" - {
+
+        "must go to UnderConstruction Page" in {
+          val userData = UserAnswers("id").withPage(AccountClosedPage(accountId), true)
+          navigator.nextPage(AccountClosedPage(accountId), NormalMode, userData) mustBe
             controllers.routes.UnderConstructionController.onPageLoad()
         }
 
