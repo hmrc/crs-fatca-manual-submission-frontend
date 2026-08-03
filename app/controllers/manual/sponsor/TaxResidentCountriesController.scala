45e2ddd788d3e5b5acef500d7f89fc1b28e89aee
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2026-08-03 18:40:43 +0100

    DAC6-4406 | implement TaxResidentCountries Page

---
+++ app/controllers/manual/sponsor/TaxResidentCountriesController.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.manual.sponsor
+
+import controllers.actions.*
+import forms.manual.sponsor.TaxResidentCountriesFormProvider
+
+import javax.inject.Inject
+import models.{Mode, ReportId}
+import navigation.ManualSubmissionNavigator
+import pages.manual.sponsor.TaxResidentCountriesPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import connectors.DatabaseConnector
+import pages.SponsorResidentForTaxPage
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.manual.sponsor.TaxResidentCountriesView
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class TaxResidentCountriesController @Inject() (
+  override val messagesApi: MessagesApi,
+  repository: DatabaseConnector,
+  navigator: ManualSubmissionNavigator,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  reportIdAction: ReportIdRequiredAction,
+  formProvider: TaxResidentCountriesFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  view: TaxResidentCountriesView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  val form = formProvider()
+
+  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData andThen reportIdAction) {
+    implicit request =>
+
+      implicit val reportId: ReportId = request.reportId
+
+      val taxResidentCountries = request.userAnswers
+        .get(SponsorResidentForTaxPage())
+        .map(_.resCountryCodes)
+        .getOrElse(Seq.empty)
+
+      val preparedForm = request.userAnswers.get(TaxResidentCountriesPage()) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, mode, taxResidentCountries))
+  }
+
+  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData andThen reportIdAction).async {
+    implicit request =>
+
+      implicit val reportId: ReportId = request.reportId
+
+      val taxResidentCountries = request.userAnswers
+        .get(SponsorResidentForTaxPage())
+        .map(_.resCountryCodes)
+        .getOrElse(Seq.empty)
+
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, mode, taxResidentCountries))),
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.setWithReportId(TaxResidentCountriesPage(), value))
+              _              <- repository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(TaxResidentCountriesPage(), mode, updatedAnswers))
+        )
+  }
+}
