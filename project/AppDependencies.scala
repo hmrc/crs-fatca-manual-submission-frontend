e5a568b42c4c64598ca5c269a2ae7188f791bd06
Carl Thompson <carl.thompson@opencastsoftware.com>
2026-08-05 11:29:09 +0100

    DAC6-4403 | update play mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.13.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
@@ -17,7 +17,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus" %% "mockito-3-4"             % "3.2.10.0",
-    "org.mockito"       %% "mockito-scala"           % "2.2.1",
+    "org.mockito"       %% "mockito-scala"           % "2.2.3",
     "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0"
   ).map(_ % Test)
 
