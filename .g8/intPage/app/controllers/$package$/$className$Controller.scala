785f376e0fd1366806f51f288032afb26b53d5ba
alikouhestani <90757950+alikouhestani@users.noreply.github.com>
2026-08-10 16:18:50 +0100

    DAC6-4421 | added package name to leftover imports

--- .g8/intPage/app/controllers/$package$/$className$Controller.scala
+++ .g8/intPage/app/controllers/$package$/$className$Controller.scala
@@ -5,12 +5,12 @@ import forms.$className$FormProvider
 import javax.inject.Inject
 import models.{Mode, ReportId}
 import navigation.ManualSubmissionNavigator
-import pages.$className$Page
+import pages.$package$.$className$Page
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import connectors.DatabaseConnector
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import views.html.$className$View
+import views.html.$package$.$className$View
 
 import scala.concurrent.{ExecutionContext, Future}
 
