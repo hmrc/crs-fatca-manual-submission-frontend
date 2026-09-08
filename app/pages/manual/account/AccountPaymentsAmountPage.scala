8ff84f8c7845b568c3ea9297a1dd39773a460de0
Carl Thompson <carl.thompson@opencastsoftware.com>
2026-09-08 17:05:24 +0100

    DAC6-4453 | address todos, introduce logger for journey recovery, update messages

--- app/pages/manual/account/AccountPaymentsAmountPage.scala
+++ app/pages/manual/account/AccountPaymentsAmountPage.scala
@@ -22,7 +22,7 @@ import models.manual.account.AccountPaymentsAmount
 import pages.QuestionPage
 import play.api.libs.json.JsPath
 
-//TODO CAN REMOVE THIS - what about navigation
+//this class is currently only used for navigation
 final case class AccountPaymentsAmountPage(accountId: AccountId)(implicit reportId: ReportId) extends QuestionPage[AccountPaymentsAmount]:
 
   override def path: JsPath = JsPath \ reportId.mongoKey \ "accounts" \ accountId.value \ "accountPaymentsAmount"
