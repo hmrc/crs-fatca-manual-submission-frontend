52c54cf2f4a421279a5d491d962b6f37e08bc998
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2026-09-09 18:27:54 +0100

    DAC6-4449 | CRS/FATCA : Create "uk-postcode" & "is-this-the-address" pages (account-holder section, Manual)

--- test-utils/generators/ModelGenerators.scala
+++ test-utils/generators/ModelGenerators.scala
@@ -23,7 +23,18 @@ import models.{CrsOrFatca, NumberType, TypeOfReport, UkAddress}
 import org.scalacheck.Arbitrary.*
 import org.scalacheck.{Arbitrary, Gen}
 
+import models.UkPostCodeForAccountHolder
+import org.scalacheck.Arbitrary.*
+
 trait ModelGenerators {
+
+  implicit lazy val arbitraryUkPostCodeForAccountHolder: Arbitrary[UkPostCodeForAccountHolder] =
+    Arbitrary {
+      for {
+        postcode <- arbitrary[String]
+        some     <- arbitrary[String]
+      } yield UkPostCodeForAccountHolder(postcode)
+    }
 
   implicit lazy val arbitraryPaymentType: Arbitrary[PaymentType] =
     Arbitrary {
