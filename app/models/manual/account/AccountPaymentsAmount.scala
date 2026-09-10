458c61b08d4bcf59e07229ac933a345c9c35c510
Carl Thompson <77161245+C-Thomp@users.noreply.github.com>
2026-09-10 16:11:33 +0100

    Dac6 4453 (#123)
    * DAC6-4453 | add AccountPaymentIndexRequiredAction
    
    * DAC6-4453 | Create page wip
    
    * DAC6-4453 | remove dupe
    
    * DAC6-4453 | refactor packages, add paymenttype view parameter, add viewspec, spec refactors and tidy up
    
    * DAC6-4453 | viewspec update
    
    * DAC6-4453 | add abstract method to PaymentType for converting the type codes into message strings for content
    
    * DAC6-4453 | remove uneeded test
    
    * DAC6-4453 | address todos, introduce logger for journey recovery, update messages
    
    * DAC6-4453 | small fixes - lowercase payment tyopes in content
    
    * DAC6-4453 | lowercase

--- app/models/manual/account/AccountPaymentsAmount.scala
+++ app/models/manual/account/AccountPaymentsAmount.scala
@@ -16,11 +16,11 @@
 
 package models.manual.account
 
-import play.api.libs.json.{Json, OFormat}
+import models.Currency
+import play.api.libs.json.*
 
-case class AccountPayment(paymentType: PaymentType, accountPaymentsAmount: Option[AccountPaymentsAmount] = None)
+case class AccountPaymentsAmount(currency: Currency, amount: String)
 
-object AccountPayment {
-  implicit val format: OFormat[AccountPayment] = Json.format
-
+object AccountPaymentsAmount {
+  implicit val format: OFormat[AccountPaymentsAmount] = Json.format[AccountPaymentsAmount]
 }
