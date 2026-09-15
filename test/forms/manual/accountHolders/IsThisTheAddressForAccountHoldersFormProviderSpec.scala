d0938b5c6f79bcbb4f24a3041b7cd821e4f0d77e
thando.mafela <thando.mafela@digital.hmrc.gov.uk>
2026-09-15 12:14:34 +0100

    DAC6-4449 | fixing validation error

--- test/forms/manual/accountHolders/IsThisTheAddressForAccountHoldersFormProviderSpec.scala
+++ test/forms/manual/accountHolders/IsThisTheAddressForAccountHoldersFormProviderSpec.scala
@@ -21,7 +21,7 @@ import play.api.data.FormError
 
 class IsThisTheAddressForAccountHoldersFormProviderSpec extends BooleanFieldBehaviours {
 
-  val requiredKey = "account.holder.uk.isThisTheAddressForAccountHolders.error.required "
+  val requiredKey = "account.holder.uk.isThisTheAddressForAccountHolders.error.required"
   val invalidKey  = "error.boolean"
 
   val form = new IsThisTheAddressForAccountHoldersFormProvider()()
